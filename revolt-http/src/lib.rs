81f27e442a66263268665f67695604afabe952d1
arslee07 <mail@arslee.me>
2022-07-24 16:47:21 +0300

    chore(http): handle API errors

--- revolt-http/src/lib.rs
+++ revolt-http/src/lib.rs
@@ -1,10 +1,22 @@
-use reqwest::{Client, Method};
+use reqwest::{Client, Method, StatusCode};
 use revolt_models::{
-    authentication::Authentication, message::Message, payload::SendMessagePayload,
+    authentication::Authentication, message::Message, payload::SendMessagePayload, ApiError,
 };
-use serde::{Deserialize, Serialize};
+use serde::Serialize;
 
-type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+type Result<T> = std::result::Result<T, RevoltHttpError>;
+
+#[derive(Debug, thiserror::Error)]
+pub enum RevoltHttpError {
+    #[error("Serde JSON serialization/deserialization error: {0}")]
+    Serialization(#[from] serde_json::Error),
+
+    #[error("Error while processing an HTTP request: {0}")]
+    HttpRequest(#[from] reqwest::Error),
+
+    #[error("Error returned from API")]
+    Api(ApiError),
+}
 
 pub struct RevoltHttp {
     base_url: String,
@@ -52,14 +64,31 @@ impl RevoltHttp {
             r = r.json(&b);
         }
 
-        Ok(r.send().await?.json().await?)
+        let resp = r.send().await?;
+        let status = resp.status();
+        let body = resp.text().await?;
+
+        if status.is_success() {
+            let json = serde_json::from_str::<R>(&body)?;
+            Ok(json)
+        } else {
+            // NOTE: it's a workaround thing but there are no alternative methods
+            // because API returns HTML instead of parseable JSON
+            // uhhhm also sorry for my broken english
+            if status == StatusCode::UNAUTHORIZED {
+                return Err(RevoltHttpError::Api(ApiError::Unauthenticated));
+            }
+
+            let json = serde_json::from_str::<ApiError>(&body)?;
+            Err(RevoltHttpError::Api(ApiError::from(json)))
+        }
     }
 
     pub async fn send_message(
         &self,
         channel_id: String,
         payload: SendMessagePayload,
-    ) -> Result<Message> {
+    ) -> Result<()> {
         self.req(
             "POST",
             format!("/channels/{}/messages", channel_id).as_str(),
