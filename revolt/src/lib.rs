f825422d2be85cd73a34fee4c9aaa4fe313afb3c
arslee07 <mail@arslee.me>
2022-08-20 13:51:20 +0300

    feat: create revolt package which re-exports all other packages

---
+++ revolt/src/lib.rs
@@ -0,0 +1,15 @@
+pub mod models {
+    pub use revolt_models::*;
+}
+
+pub mod http {
+    pub use revolt_http::*;
+}
+
+pub mod gateway {
+    pub use revolt_gateway::*;
+}
+
+pub mod util {
+    pub use revolt_util::*;
+}
